34c9a766057185b1a694404b7d21f464cd2d1422
poello <64276346+poello@users.noreply.github.com>
2020-06-10 17:57:11 +0200

    Update Locations.java

--- Locations.java
+++ Locations.java
@@ -7,58 +7,7 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
     private static Map<Integer, IndexRecord> index = new LinkedHashMap<>();
     private static RandomAccessFile ra;
-
-    public static void main(String[] args) throws IOException{
-        try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
-            rao.writeInt(locations.size()); // 1. step
-            int indexSize = locations.size() * 3 * Integer.BYTES;
-            int locationStart = (int) (indexSize + rao.getFilePointer() + Integer.BYTES);
-            rao.writeInt(locationStart); // 2. step
-
-            long indexStart = rao.getFilePointer();
-
-            int startPointer = locationStart;
-            rao.seek(startPointer); // set the offset (location in the file)
-
-            for(Location location : locations.values()) {
-                rao.writeInt(location.getLocationID());
-                rao.writeUTF(location.getDescription());
-                StringBuilder builder = new StringBuilder();
-
-                for(String direction : location.getExits().keySet()) {
-                    if (!direction.equalsIgnoreCase("Q")) {
-                        builder.append(direction);
-                        builder.append(",");
-                        builder.append(location.getExits().get(direction));
-                        builder.append(",");
-                        // direction, locationID, direction, locationID
-                        // N,1,W,3
-                    }
-                }
-                rao.writeUTF(builder.toString());
-
-                IndexRecord record = new IndexRecord(startPointer, (int) (rao.getFilePointer() - startPointer));
-                index.put(location.getLocationID(), record);
-
-                startPointer = (int) rao.getFilePointer();
-            }
-
-
-            rao.seek(indexStart);
-            for(Integer locationID : index.keySet()) {
-                rao.writeInt(locationID);
-                rao.writeInt(index.get(locationID).getStartByte());
-                rao.writeInt(index.get(locationID).getLength());
-            }
-
-        }
-
-    }
-
-    // 1. This first four bytes will contain the number of locations (bytes 0-3)
-    // 2. The next four bytes will contain the start offset of the locations section (bytes 4-7)
-    // 3. The next section of the file will contain the index (The index is 1692 bytes long. It will start at byte 8 and end at byte 1699
-    // 4. The final section of the file will contain the location records (the data). It will start at byte 1700
+    
     static {
         try {
             ra = new RandomAccessFile("locations_rand.dat", "rwd");
